b8a725ee7db9af8ff8301ddcef285c5a0bb47e38
Jaakko Korhonen <jaakko.korhonen@gmail.com>
2026-07-04 13:33:34 +0300

    refactor: extract code samples from TECHNICAL_DESIGN.md into own files

---
+++ scripts/test-webhook.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Manuaalinen curl-testi Jira Automation -webhookille
+# Käyttö: JIRA_WEBHOOK_URL=... JIRA_WEBHOOK_TOKEN=... ./scripts/test-webhook.sh
+
+curl -s -X POST \
+  "${JIRA_WEBHOOK_URL}" \
+  -H "Content-Type: application/json" \
+  -H "X-Automation-Webhook-Token: ${JIRA_WEBHOOK_TOKEN}" \
+  -d '{
+    "action": "opened",
+    "issue": {
+      "number": 999,
+      "title": "Test",
+      "body": "Test body",
+      "html_url": "https://github.com/uutisseuranta/uutisseuranta.github.io/issues/999"
+    },
+    "repository": {
+      "name": "uutisseuranta.github.io"
+    }
+  }' \
+  -w "\nHTTP %{http_code}\n"
